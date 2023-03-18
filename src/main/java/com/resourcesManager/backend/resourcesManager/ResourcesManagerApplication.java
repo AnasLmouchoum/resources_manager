1894dfc1dc2638b25bf139faee8270785dd0370f
AnasLmouchoum <95775026+AnasLmouchoum@users.noreply.github.com>
2023-03-18 15:03:25 +0100

    initial commit

---
+++ src/main/java/com/resourcesManager/backend/resourcesManager/ResourcesManagerApplication.java
@@ -0,0 +1,13 @@
+package com.resourcesManager.backend.resourcesManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResourcesManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResourcesManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/resourcesManager/backend/resourcesManager/ResourcesManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.resourcesManager.backend.resourcesManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResourcesManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
